dc6bebb4740aaa72846ead9fe10fbc1a8e778f2f
KDMashy <kagethk@gmail.com>
2022-04-10 16:40:41 +0200

    NPC save
    fixed NPC save error

--- ShimaKeeCSharp/ShimaKeeCSharp/entity/npc/NPCFunctions.cs
+++ ShimaKeeCSharp/ShimaKeeCSharp/entity/npc/NPCFunctions.cs
@@ -8,20 +8,6 @@ public class NPCFunctions
 {
     public void CreateNPC(Player player)
     {
-        SaveFunc(false, false, player.Name);
-    }
-
-    public void SaveNPC(Player player, List<NPC> npcs)
-    {
-        foreach (NPC npc in npcs)
-        {
-            SaveFunc(npc.Completed, npc.Accepted, player.Name);
-        }
-    }
-
-    private void SaveFunc(bool completed, bool accepted, string player)
-    {
-        string fileName = player + "_NPC.txt";
         string[] npcNames = new string[]
         {
             "Mashiron", "Shiina",
@@ -29,14 +15,28 @@ public class NPCFunctions
             "Chi", "Shiro",
             "Kuro", "Hiro"
         };
-        using (var fileStream = new FileStream(fileName, FileMode.OpenOrCreate))
+        foreach (string npcName in npcNames)
+        {
+            SaveFunc(npcName, false, false, player.Name);
+        }
+    }
+
+    public void SaveNPC(Player player, List<NPC> npcs)
+    {
+        DeleteNPCList(player);
+        foreach (NPC npc in npcs)
+        {
+            SaveFunc(npc.Name, npc.Completed, npc.Accepted, player.Name);
+        }
+    }
+
+    private void SaveFunc(string npcName, bool completed, bool accepted, string player)
+    {
+        string fileName = player + "_NPC.txt";
+        using (var fileStream = new FileStream(fileName, FileMode.Append))
         using (var writer = new StreamWriter(fileStream))
         {
-            writer.Write($"{npcNames[0]};{completed};{accepted};{player}");
-            for (int i = 1; i < 8; i++)
-            {
-                writer.Write($":{npcNames[i]};{completed};{accepted};{player}");
-            }
+            writer.Write($"{npcName};{completed};{accepted};{player}:");
         }
     }
 
@@ -52,24 +52,27 @@ public class NPCFunctions
                 string[] npcs = line.Split(':');
                 foreach (string read in npcs)
                 {
-                    string[] npcData = read.Split(';');
+                    if (!read.Equals(""))
+                    {
+                        string[] npcData = read.Split(';');
                     
-                    bool completed = false;
-                    bool accepted = false;
+                        bool completed = false;
+                        bool accepted = false;
                     
-                    if (npcData[1].Contains("True")) completed = true;
-                    else completed = false;
+                        if (npcData[1].Contains("True")) completed = true;
+                        else completed = false;
                     
-                    if (npcData[2].Contains("True")) accepted = true;
-                    else accepted = false;
+                        if (npcData[2].Contains("True")) accepted = true;
+                        else accepted = false;
                     
-                    NPC newNPC = new NPC(
-                        npcData[0],
-                        completed,
-                        accepted,
-                        npcData[3]);
+                        NPC newNPC = new NPC(
+                            npcData[0],
+                            completed,
+                            accepted,
+                            npcData[3]);
                     
-                    NPCList.Add(newNPC);
+                        NPCList.Add(newNPC);
+                    }
                 }
             }
         }
@@ -77,12 +80,12 @@ public class NPCFunctions
         return NPCList;
     }
 
-    /*public void DeleteNPCList(Player player)
+    public void DeleteNPCList(Player player)
     {
         string fileName = player.Name + "_NPC.txt";
         if(File.Exists(fileName))
         {
             File.Delete(fileName);
         }
-    }*/
+    }
 }
